0e3199be46bc1ae9dd22769c8b101adbf42c32ec
tbouchnafa <ein-langer-weg@hotmail.de>
2014-09-02 11:30:31 +0200

    Adding Similarity Classes

--- src/main/java/de/mft/MFTPage.java
+++ src/main/java/de/mft/MFTPage.java
@@ -1,19 +1,32 @@
 package de.mft;
 
 import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.form.AjaxFormSubmitBehavior;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextArea;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.model.Model;
+
+import de.mft.similarity.WS4JManager;
 
 public class MFTPage extends WebPage {
 
 	private static final long serialVersionUID = 1L;
 
+	WS4JManager ws4j = new WS4JManager();
+	
+	
 	public MFTPage(final PageParameters parameters) {
 		super(parameters);
 
-		add(new Label("version", getApplication().getFrameworkSettings().getVersion()));
-
-		// TODO Add your page's components here
-
+    	
+    	FeedbackPanel feedbackPanel = new FeedbackPanel("feedback");
+        feedbackPanel.setOutputMarkupId(true);
+    	add(feedbackPanel);
+         
     }
 }
